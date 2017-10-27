d7a4e0af5bf9aed0cd474e6a01523279bf2af5f5
RickyRom <30517351+RickyRom@users.noreply.github.com>
2017-10-27 17:02:25 -0600

    avance del main

--- Lab3_RichardPadgett_RicardoRomero/src/lab3_richardpadgett_ricardoromero/Clientes.java
+++ Lab3_RichardPadgett_RicardoRomero/src/lab3_richardpadgett_ricardoromero/Clientes.java
@@ -2,28 +2,23 @@
 package lab3_richardpadgett_ricardoromero;
 
 public class Clientes extends Personas{
-    int productocomp;
+    String productocomp;
     int dinero;
 
     public Clientes() {
     }
 
-    public Clientes(int productocomp, int dinero) {
-        this.productocomp = productocomp;
-        this.dinero = dinero;
-    }
-
-    public Clientes(int productocomp, int dinero, String user, String passw, String email, String nombrec, String id, String fechan) {
+    public Clientes(String productocomp, int dinero, String user, String passw, String email, String nombrec, String id, String fechan) {
         super(user, passw, email, nombrec, id, fechan);
         this.productocomp = productocomp;
         this.dinero = dinero;
     }
 
-    public int getProductocomp() {
+    public String getProductocomp() {
         return productocomp;
     }
 
-    public void setProductocomp(int productocomp) {
+    public void setProductocomp(String productocomp) {
         this.productocomp = productocomp;
     }
 
@@ -81,12 +76,10 @@ public class Clientes extends Personas{
 
     public void setFechan(String fechan) {
         this.fechan = fechan;
-    }
+    }    
 
     @Override
     public String toString() {
         return "Clientes{" + "productocomp=" + productocomp + ", dinero=" + dinero + '}';
     }
-    
-    
 }
