d193481d1c684113177530fba43b19f9d730452b
Richard Padgett <richard.padgett@hotmail.com>
2017-10-27 14:12:44 -0600

    pull de clases

--- Lab3_RichardPadgett_RicardoRomero/src/lab3_richardpadgett_ricardoromero/Lab3_RichardPadgett_RicardoRomero.java
+++ Lab3_RichardPadgett_RicardoRomero/src/lab3_richardpadgett_ricardoromero/Lab3_RichardPadgett_RicardoRomero.java
@@ -1,12 +1,36 @@
-
 package lab3_richardpadgett_ricardoromero;
 
-public class Lab3_RichardPadgett_RicardoRomero {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Tiendas");
-        System.out.println("1.Comprar");
-        System.out.println("2.Administrar");
-    }
+public class Lab3_RichardPadgett_RicardoRomero {
     
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int op1 = 0;
+        while (op1 <= 5) {
+            System.out.println("------Super Mercado------");
+            System.out.println("1.Iniciar Sesion Clientes");
+            System.out.println("2.Iniciar Sesion Empleado");
+            System.out.println("3.Registrar Empleados");
+            System.out.println("4.Registrarse como cliente");
+            op1 = sc.nextInt();
+            if (op1 == 1) {
+                
+            } else if (op1 == 2) {
+                System.out.println("Ingrese su nombre de usuaro");
+                String us = sc.next();
+                System.out.println("ingrese su contra su password");
+                String pass = sc.next();
+                if (us == "Sociodelaempresa13"&&pass == "XRL8") {
+                    System.out.println("--Bienvenido--");
+                    System.out.println("");
+                }
+            } else if (op1 == 3) {
+                
+            } else if (op1 == 4) {
+                
+            }
+        }
+    }
 }
