f760b8b2ebe42183a278c365b7eddcbdd335a18a
Richard Padgett <richard.padgett@hotmail.com>
2017-10-27 14:01:25 -0600

    Creacion subclase empleados y Clientes

--- Lab3_RichardPadgett_RicardoRomero/src/lab3_richardpadgett_ricardoromero/Lab3_RichardPadgett_RicardoRomero.java
+++ Lab3_RichardPadgett_RicardoRomero/src/lab3_richardpadgett_ricardoromero/Lab3_RichardPadgett_RicardoRomero.java
@@ -4,7 +4,9 @@ package lab3_richardpadgett_ricardoromero;
 public class Lab3_RichardPadgett_RicardoRomero {
 
     public static void main(String[] args) {
-     
+        System.out.println("Tiendas");
+        System.out.println("1.Comprar");
+        System.out.println("2.Administrar");
     }
     
 }
